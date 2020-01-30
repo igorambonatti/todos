9627e07e83e9b8660e5ca1a370ecec51fcb13240
Igor ambonatti <contatoambonatti@gmail.com>
2020-01-30 14:42:39 -0300

    Estrutura

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Router } from 'react-router-dom';
+import GlobalStyle from './styles/global';
+
+import history from './services/history';
+
+import Routes from './routes/index';
+
+function App() {
+  return (
+    <Router history={history}>
+      <GlobalStyle />
+      <Routes />
+    </Router>
+  );
+}
+
+export default App;
