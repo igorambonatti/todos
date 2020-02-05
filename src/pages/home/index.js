dd4c55c21ba782408db1347f2d8e93186635bff6
Igor ambonatti <contatoambonatti@gmail.com>
2020-02-04 21:52:49 -0300

    Request Todos

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Container, Content } from './styles';
 
+import api from '../../services/api';
+
 export default function Home() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    async function Load() {
+      const response = await api.get(
+        'https://jsonplaceholder.typicode.com/todos'
+      );
+      setTodos(response.data);
+    }
+    Load();
+  }, []);
   return (
     <Container>
+      {console.log(todos)}
       <Content />
     </Container>
   );
